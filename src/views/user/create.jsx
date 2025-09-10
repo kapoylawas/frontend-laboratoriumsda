ff9359cca5a529c695202235c3722448fbd8c580
kapoylawas <arif.sangga@gmail.com>
2025-09-10 11:30:08 +0700

    update edit delete and active user

---
+++ src/views/user/create.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
+import Api from "../../services/api";
+import { handleErrors } from "../../utils/handleErrors";
+
+
+export default function CategoryCreate({ fetchData }) {
+
+}
