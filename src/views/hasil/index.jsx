3bc1659ff26ccc3c9f5c5a89f76d08c065bc4012
kapoylawas <arif.sangga@gmail.com>
2025-10-30 11:17:45 +0700

    update header terbaru

---
+++ src/views/hasil/index.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+//import layout admin
+import LayoutAdmin from "../../layouts/admin";
+
+//import js cookie
+import Cookies from "js-cookie";
+
+//import service api
+import Api from "../../services/api";
+
+
+export default function HasilIndex() {
+
+    return (
+        <LayoutAdmin>
+
+        </LayoutAdmin>
+    )
+}
